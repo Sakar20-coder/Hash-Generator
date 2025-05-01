63527ca5fdebf4f77c4da15ec36f48d6017f56c6
Sakar20-coder <sakarmishra0@gmail.com>
2025-05-01 07:57:49 +0000

    Added EJS view, updated hash.js, and created .gitignore

--- hash.js
+++ hash.js
@@ -1,1 +1,23 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', { hash: null });
+});
+
+app.post('/hash', (req, res) => {
+  const inputText = req.body.inputText || '';
+  const hash = crypto.createHash('sha256').update(inputText).digest('hex');
+  res.render('index', { hash });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🌐 Server running at http://localhost:${PORT}`));
 
